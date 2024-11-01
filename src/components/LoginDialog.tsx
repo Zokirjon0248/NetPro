3d7ce0c812bcc23877c5fc3c4601fed69e30beee
Nematov Zokirjon <z.nematov.uz@gmail.com>
2024-11-01 21:03:30 +0500

    Hero full done

--- src/components/LoginDialog.tsx
+++ src/components/LoginDialog.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react"
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
@@ -14,7 +15,9 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { ArrowRight } from "lucide-react"
-
+import { toast } from "sonner"
+import { Toaster } from "./ui/sonner";
+import { IoLogoYoutube } from "react-icons/io";
 export default function LoginDialog() {
 
   const [formData, setFormData] = useState({
@@ -41,67 +44,92 @@ export default function LoginDialog() {
     e.preventDefault()
     if (isFormValid) {
       console.log('Forma yuborildi:', formData)
+      toast.success('Forma muvaffaqiyatli yuborildi!')
+      setFormData({
+        name: '',
+        phone: '',
+        email: ''
+      })
     }
   }
-
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">Ro'yxatdan o'tish</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle className="text-center">Ro'yxatdan o'tish</DialogTitle>
-          <DialogDescription className="text-center">
-            Tasdiqlash kodi ko'rsatilgan raqamga bepul SMS orqali yuboriladi
-          </DialogDescription>
-        </DialogHeader>
-        <form onSubmit={handleSubmit}>
-          <div className="grid gap-4 py-4">
-            <div className="grid gap-2">
-              <Label htmlFor="name">Isim</Label>
-              <Input
-                id="name"
-                name="name"
-                placeholder="Ismingizni kiriting"
-                required
-                value={formData.name}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="phone">Telefon raqam</Label>
-              <Input
-                id="phone"
-                name="phone"
-                type="tel"
-                placeholder="+998 __ ___ __ __"
-                required
-                value={formData.phone}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="Email manzilingizni kiriting"
-                required
-                value={formData.email}
-                onChange={handleInputChange}
-              />
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit" className="w-full" disabled={!isFormValid}>
+    <>
+      <Toaster position="top-right" />
+      <div className="mt-[30px] flex items-center justify-center lg:justify-start gap-[40px] flex-wrap">
+
+        <Dialog>
+          <DialogTrigger asChild>
+            <button className="flex items-center gap-3 bg-custom-gradient px-[35px] py-[20px] rounded-md transition-transform duration-300 hover:scale-95 active:scale-105">
               Kursga yozilish
-              <ArrowRight className="ml-2 h-4 w-4" />
-            </Button>
-          </DialogFooter>
-        </form>
-      </DialogContent>
-    </Dialog>
+              <img src="/rightBtn.png" width={17.5} height={13.5} alt="" />
+            </button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle className="text-center">Ro'yxatdan o'tish</DialogTitle>
+              <DialogDescription className="text-center">
+                Tasdiqlash kodi ko'rsatilgan raqamga bepul SMS orqali yuboriladi
+              </DialogDescription>
+            </DialogHeader>
+            <form onSubmit={handleSubmit}>
+              <div className="grid gap-4 py-4">
+                <div className="grid gap-2">
+                  <Label className="text-black" htmlFor="name">Isim</Label>
+                  <Input
+                    className="border-[#434343]  text-black"
+                    id="name"
+                    name="name"
+                    placeholder="Ismingizni kiriting"
+                    required
+                    value={formData.name}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label className="text-black" htmlFor="phone">Telefon raqam</Label>
+                  <Input
+                    className="border-[#434343]  text-black"
+                    id="phone"
+                    name="phone"
+                    type="tel"
+                    placeholder="+998 __ ___ __ __"
+                    required
+                    value={formData.phone}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label className="text-black" htmlFor="email">Email</Label>
+                  <Input
+                    className="border-[#434343]  text-black"
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="Email manzilingizni kiriting"
+                    required
+                    value={formData.email}
+                    onChange={handleInputChange}
+                  />
+                </div>
+              </div>
+              <DialogFooter>
+                <DialogClose disabled={!isFormValid}>
+                  <Button type="submit" className="w-full" disabled={!isFormValid}>
+                    Kursga yozilish
+                    <ArrowRight className="ml-2 h-4 w-4" />
+
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </form>
+          </DialogContent>
+        </Dialog>
+          <a href="http://www.youtube.com/@netproitschool9325" className="px-[40px] py-[20px]  flex items-center gap-3 bg-custom-gradient cursor-pointer rounded-md transition-transform duration-300 hover:scale-95 active:scale-105" target='_blank'>
+            Youtube
+            <IoLogoYoutube className="text-2xl" />
+          </a>
+      </div>
+    </>
+
   )
 }
